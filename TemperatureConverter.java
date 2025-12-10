80f1eb9fb377ea1552f776cd33391add0c51c4af
Shubham Sharma <sharmashubham05275@gmail.com>
2025-12-10 21:25:58 +0530

    Add files via upload

---
+++ TemperatureConverter.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class TemperatureConverter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter temperature in Fahrenheit: ");
+        double fahrenheit = sc.nextDouble();
+
+        double celsius = (fahrenheit - 32) * 5 / 9;
+
+        System.out.printf("Temperature in Celsius: %.2f°C", celsius);
+        sc.close();
+    }
+}
